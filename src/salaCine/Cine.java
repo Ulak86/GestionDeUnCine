3332082d54ce378b8c1d7cb7ac45ac7a215fd0da
Ulak86_Eclipse_Getafe <Tardes@GET-11>
2025-09-11 19:37:44 +0200

    Clases Cine y Usuario

---
+++ src/salaCine/Cine.java
@@ -0,0 +1,14 @@
+package salaCine;
+
+public class Cine {
+
+		String nombre;
+		SalaDeCine[] salas;
+		
+		public Cine(String nombre, SalaDeCine[] salas) {
+			this.nombre = nombre;
+			this.salas = salas;
+		}
+		
+		
+}
---
+++ src/salaCine/Usuario.java
@@ -0,0 +1,17 @@
+package salaCine;
+
+public class Usuario {
+	
+	private String nombre;
+	private String dni;
+	private int edad;
+	
+	public Usuario(String nombre, String dni, int edad) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.edad = edad;
+	}
+	
+	
+
+}
