070f27a9f44412b61c0ab3182a9675ebc714cd0e
Ulak86_Eclipse_Getafe <Tardes@GET-11>
2025-09-11 19:41:09 +0200

    Pelicula modificada (gets y sets duera)

--- src/salaCine/Pelicula.java
+++ src/salaCine/Pelicula.java
@@ -1,14 +1,13 @@
 package salaCine;
 
 public class Pelicula {
-	
+
 	private String titulo;
 	private int duracion;
 	private int clasificacionEdad;
 	private double precio;
 	private int id;
-	
-	
+
 	public Pelicula(String titulo, int duracion, int clasificacionEdad, double precio, int id) {
 		super();
 		this.titulo = titulo;
@@ -17,57 +16,5 @@ public class Pelicula {
 		this.precio = precio;
 		this.id = id;
 	}
-
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-
-	public int getDuracion() {
-		return duracion;
-	}
-
-
-	public void setDuracion(int duracion) {
-		this.duracion = duracion;
-	}
-
-
-	public int getClasificacionEdad() {
-		return clasificacionEdad;
-	}
-
-
-	public void setClasificacionEdad(int clasificacionEdad) {
-		this.clasificacionEdad = clasificacionEdad;
-	}
-
-
-	public double getPrecio() {
-		return precio;
-	}
-
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-
-
-	public int getId() {
-		return id;
-	}
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-		
 
 }
