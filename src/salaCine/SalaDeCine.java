c0e37afdc65e7ca85995168058923b6ad19645be
Tardes <Tardes@CAS-GT0717>
2025-09-11 19:39:11 +0200

    clases

--- src/salaCine/SalaDeCine.java
+++ src/salaCine/SalaDeCine.java
@@ -1,5 +1,21 @@
 package salaCine;
 
 public class SalaDeCine {
+	
+	private String nombre;
+	private int capacidad;
+	private String peliculaProyectada;
+	private Usuario [][] butacas;
+	
+	
+	public SalaDeCine(String nombre, int capacidad, String peliculaProyectada, Usuario[][] butacas) {
+		super();
+		this.nombre = nombre;
+		this.capacidad = capacidad;
+		this.peliculaProyectada = peliculaProyectada;
+		this.butacas = butacas;
+	}
 
+
+	
 }
