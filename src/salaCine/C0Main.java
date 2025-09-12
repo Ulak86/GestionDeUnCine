cb4d2e361f492820a5da6839e767f110f6e587d2
Ulak86_Eclipse_Getafe <Tardes@GET-11>
2025-09-12 20:35:15 +0200

    C0Main

---
+++ src/salaCine/C0Main.java
@@ -0,0 +1,15 @@
+package salaCine;
+
+import java.util.Scanner;
+
+public class C0Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		bienvenidoAlCine.moloMucho(sc);
+		
+		sc.close();
+	}
+
+}
